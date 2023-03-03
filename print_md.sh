05ef530ca1694166c461fc588adb0dfb0a067f23
ryonakennedy <53537687+ryonakennedy@users.noreply.github.com>
2023-03-03 08:05:42 -0500

    reviewChanges

--- print_md.sh
+++ print_md.sh
@@ -1,2 +1,4 @@
-.lpstat -p -d
-find . -regex '.*\.md' -exec lp -d <printer_name> {} \;
+# List of printer names
+lpstat -p -d
+# Print all md recursively
+find .  -regex '.*\.md' -exec lp -d <printer_name> {} \;
